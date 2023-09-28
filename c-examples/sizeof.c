db2329ec05522ef1b60ea06ccdb8c9a26f378021
Brian <brian030128@gmail.com>
2023-09-28 17:25:12 +0800

    update sizeof

--- c-examples/sizeof.c
+++ c-examples/sizeof.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
+#include<stdbool.h>
 
 
 int main(){
-    int a = 1;
-    printf("%lu",sizeof(a));
-    printf("%lu",sizeof(a));
-    printf("%lu",sizeof(a));
-    printf("%lu",sizeof(a));
+    printf("%lu",sizeof(1)); //4
+    short int a = 1;
+    printf("%lu",sizeof(a)); //2
+    printf("%lu",sizeof(false)); //4
+    printf("%lu",sizeof(2L));  //8
 }
+
+//Output: 4248
